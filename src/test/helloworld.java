337fd5ccbb2997911e06587ae16ba7c12e586fb2
LBoy <730853604@qq.com>
2026-06-25 21:58:58 +0800

    我的第一个Java项目提交

---
+++ src/test/helloworld.java
@@ -0,0 +1,15 @@
+package test;
+//package:表示当前的类在哪个包下
+
+//class:表示定义一个类，类是Java项目的最小组成单元
+//helloworld:类的名字
+//{}:表示类的开始和结束，范围
+public class helloworld {
+
+
+//    表示Java程序的主入口
+//    固定格式
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
